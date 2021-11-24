a8b31d0eae572064bf7987774b42732f7ebacc91
filipeclduarte <filipe_pb_duarte@hotmail.com>
2021-11-24 19:54:50 -0300

    update

--- example/testePSO.jl
+++ example/testePSO.jl
@@ -22,18 +22,5 @@ function testPSOthreads(n::Int64, iterations::Int64)
     end
 end
 
-function testPSOsimd(n::Int64, iterations::Int64)
-    @simd for i in 1:n
-        pso = Pso(30, 5, 0.75, 1.5, 1.5, -5.12, 5.12)
-        X, gBest, gBestValue = optimize(pso, f, iterations)
-    end
-end
-
 @btime testPSO(10, 100)
 @btime testPSOthreads(10, 100)
-@btime testPSOsimd(10, 100)
-# testando threads 
-## 1 thread
-Threads.@threads for N = 1:20
-    println("N = $N (thread $(Threads.threadid()) of out $(Threads.nthreads()))")
-end
